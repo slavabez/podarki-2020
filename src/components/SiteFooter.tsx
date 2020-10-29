218478106a63170bbb16f9448e77eb7fa151ac06
Slava Bezgachev <bezgachev@gmail.com>
2020-10-29 11:09:41 +0600

    Header looks ok on all platforms

--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -68,7 +68,7 @@ const Divider = styled.div`
 const RightHalf = styled.div``;
 
 const SiteFooter = () => (
-  <Footer>
+  <Footer id="footer">
     <AddressBlock>
       <AddressTop>
         <AddressIconContainer>
