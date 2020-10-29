93671fda336d391329600de83861658163a5241c
Slava Bezgachev <bezgachev@gmail.com>
2020-10-29 15:45:57 +0600

    Initial styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,78 +1,83 @@
 import * as React from "react";
 import { graphql } from "gatsby";
 import { Helmet } from "react-helmet";
+import SimpleReactLightbox from "simple-react-lightbox";
 import Layout from "../components/Layout";
 import Gallery from "../components/PresetsGallery";
+import { dissectPresentImages } from "../utils/helpers"
 
 export const query = graphql`
   query {
-    allFile(filter: { extension: { eq: "pdf" } }) {
+    allFile(
+      filter: {
+        sourceInstanceName: { eq: "podarki" }
+        extension: { eq: "jpg" }
+      }
+    ) {
       nodes {
         relativePath
-        publicURL
         prettySize
-      }
-    }
-    file(relativePath: { eq: "2021_image.png" }) {
-      relativePath
-      childImageSharp {
-        fluid(maxHeight: 400) {
-          aspectRatio
-          base64
-          originalImg
-          originalName
-          presentationHeight
-          presentationWidth
-          sizes
-          src
+        childImageSharp {
+          fluid {
+            src
+            srcSet
+            base64
+            aspectRatio
+            originalImg
+            sizes
+          }
         }
       }
     }
   }
 `;
 
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
+  const imageData = dissectPresentImages(data?.allFile?.nodes);
+  console.log(imageData);
   return (
-    <Layout>
-      <Helmet>
-        <meta name="description" content="" />
-        <title>Сказка - Новогодние подарки и кульки 2021</title>
+    <SimpleReactLightbox>
+      <Layout>
+        <Helmet>
+          <meta name="description" content="" />
+          <title>Сказка - Новогодние подарки и кульки 2021</title>
 
-        <meta
-          name="title"
-          content="Сказка - Новогодние подарки и кульки 2021"
-        />
-        <meta
-          name="description"
-          content="Детские новогодние подарки, новогодние кульки со сладостями в г. Кокшетау, г. Костанай и г. Петропавловск. Новогодние кульки с конфетами и шоколадом из Казахстана и России"
-        />
+          <meta
+            name="title"
+            content="Сказка - Новогодние подарки и кульки 2021"
+          />
+          <meta
+            name="description"
+            content="Детские новогодние подарки, новогодние кульки со сладостями в г. Кокшетау, г. Костанай и г. Петропавловск. Новогодние кульки с конфетами и шоколадом из Казахстана и России"
+          />
 
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://skazka-podarki.kz/" />
-        <meta
-          property="og:title"
-          content="Сказка - Новогодние подарки и кульки 2021"
-        />
-        <meta
-          property="og:description"
-          content="Детские новогодние подарки, новогодние кульки со сладостями в г. Кокшетау, г. Костанай и г. Петропавловск. Новогодние кульки с конфетами и шоколадом из Казахстана и России"
-        />
-        <meta property="og:image" content="" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://skazka-podarki.kz/" />
+          <meta
+            property="og:title"
+            content="Сказка - Новогодние подарки и кульки 2021"
+          />
+          <meta
+            property="og:description"
+            content="Детские новогодние подарки, новогодние кульки со сладостями в г. Кокшетау, г. Костанай и г. Петропавловск. Новогодние кульки с конфетами и шоколадом из Казахстана и России"
+          />
+          <meta property="og:image" content="" />
 
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://skazka-podarki.kz/" />
-        <meta
-          property="twitter:title"
-          content="Сказка - Новогодние подарки и кульки 2021"
-        />
-        <meta
-          property="twitter:description"
-          content="Детские новогодние подарки, новогодние кульки со сладостями в г. Кокшетау, г. Костанай и г. Петропавловск. Новогодние кульки с конфетами и шоколадом из Казахстана и России"
-        />
-        <meta property="twitter:image" content="" />
-      </Helmet>
-      <Gallery />
-    </Layout>
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://skazka-podarki.kz/" />
+          <meta
+            property="twitter:title"
+            content="Сказка - Новогодние подарки и кульки 2021"
+          />
+          <meta
+            property="twitter:description"
+            content="Детские новогодние подарки, новогодние кульки со сладостями в г. Кокшетау, г. Костанай и г. Петропавловск. Новогодние кульки с конфетами и шоколадом из Казахстана и России"
+          />
+          <meta property="twitter:image" content="" />
+        </Helmet>
+        <Gallery imageData={imageData} />
+      </Layout>
+    </SimpleReactLightbox>
   );
 };
 
