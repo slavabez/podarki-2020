5d192c9a4958929ce6588e7fa6b88b2c74adcfbb
Slava Bezgachev <bezgachev@gmail.com>
2020-11-14 22:21:49 +0600

    Added gle tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,6 +56,21 @@ module.exports = {
         dsn: "https://132b3113300544e1b4fd1b2c9f409255@o217771.ingest.sentry.io/5505464",
         sampleRate: 1.0
       }
-    }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-69266859-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+      },
+    },
   ],
 };
